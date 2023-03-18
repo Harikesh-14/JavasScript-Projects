4a6570cbd1ef1746000612e691075ebd2475d23a
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-03-18 23:17:54 +0530

    Random Color Generator

---
+++ RandomColorGenerator/script.js
@@ -0,0 +1,27 @@
+let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+let generateBtn = document.querySelector("#genBtn")
+const colorCode = document.querySelector("#colorCode")
+const colorHex = document.querySelector(".colorHex")
+const colorBox = document.querySelector(".colorBox")
+
+generateBtn.addEventListener("click", () => {
+    const randNum = Math.floor(Math.random() * 50) + 1
+    document.body.style.backgroundColor = colorArray[randNum]
+    
+    colorCode.innerHTML = `${colorArray[randNum]}`
+    colorCode.style.color = `${colorArray[randNum]}`
+
+    colorHex.style.borderColor = `${colorArray[randNum]}`
+
+    colorBox.style.backgroundColor = `${colorArray[randNum]}`
+})
